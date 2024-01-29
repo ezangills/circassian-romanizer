e87fc4c99d4667cf4dc0d6f5ea03a291bdc5f7aa
Цеев Черим 29108 RU <cherim.tseev@dpd.ru>
2024-01-29 05:47:16 +0400

    added bot

--- src/main/java/org/tsei/romanizer/CircassianRomanizerApplication.java
+++ src/main/java/org/tsei/romanizer/CircassianRomanizerApplication.java
@@ -2,12 +2,19 @@ package org.tsei.romanizer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 public class CircassianRomanizerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CircassianRomanizerApplication.class, args);
+    public static void main(String[] args) throws TelegramApiException {
+        ApplicationContext ctx = SpringApplication.run(CircassianRomanizerApplication.class, args);
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(ctx.getBean("romanizerBot", TelegramLongPollingBot.class));
     }
 
 }
---
+++ src/main/java/org/tsei/romanizer/bot/RomanizerBot.java
@@ -0,0 +1,47 @@
+package org.tsei.romanizer.bot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.tsei.romanizer.service.RomanizerService;
+
+@Slf4j
+@Component
+public class RomanizerBot extends TelegramLongPollingBot {
+    private final RomanizerService romanizerService;
+
+    public RomanizerBot(@Autowired RomanizerService romanizerService,
+                        @Value("${bot-token}") String botToken) {
+        super(botToken);
+        this.romanizerService = romanizerService;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+            if (messageText.startsWith("/romanize")) {
+                try {
+                    SendMessage message = new SendMessage();
+                    message.setChatId(chat_id);
+                    message.setText(romanizerService.romanize(messageText));
+                    execute(message);
+                } catch (TelegramApiException e) {
+                    log.error("Could not romanize: " + messageText);
+                }
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "circassianromanizer_bot";
+    }
+}
