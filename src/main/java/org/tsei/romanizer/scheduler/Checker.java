bb5195d177d350d37b52e9bd19cf996ab15dc66a
Цеев Черим 29108 RU <cherim.tseev@dpd.ru>
2024-01-29 12:14:17 +0400

    scheduled fixes

--- src/main/java/org/tsei/romanizer/scheduler/Checker.java
+++ src/main/java/org/tsei/romanizer/scheduler/Checker.java
@@ -2,6 +2,7 @@ package org.tsei.romanizer.scheduler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -12,12 +13,13 @@ import org.tsei.romanizer.dto.RomanizerResponse;
 @Slf4j
 public class Checker {
 
-    @Value("${bot-token}") private String url;
+    @Value("${url}") private String url;
 
     @Scheduled(fixedRate = 10000)
     public void callItself() {
-        RomanizerResponse romanizerResponse = new RestTemplate().postForObject(url, new RomanizerRequest("Ти Мыекъуапэ къэлэ дах."), RomanizerResponse.class);
-        log.info("Scheduled: " + romanizerResponse.getText());
+        ResponseEntity<String> romanizerResponseResponseEntity = new RestTemplate().postForEntity(url, new RomanizerRequest("Ти Мыекъуапэ къэлэ дах."), String.class);
+        log.info("Scheduled status code: " + romanizerResponseResponseEntity.getStatusCode());
+        log.info("Scheduled body: " + romanizerResponseResponseEntity.getBody());
     }
 
 }
